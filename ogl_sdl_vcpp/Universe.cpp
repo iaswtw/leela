b5d3f6efd718ddd872898dd255deb0b4bd396fad
Abhir Joshi <Abhir_Joshi@Starkey.com>
2019-09-10 13:12:58 -0500

    Clear color to perfect black.

--- ogl_sdl_vcpp/Universe.cpp
+++ ogl_sdl_vcpp/Universe.cpp
@@ -1151,8 +1151,7 @@ void Universe::Moon_SetOrbitalPositionAngle(double fAngle)
 **************************************************************************************************/
 void Universe::render()
 {
-    //glClearDepth(1.0);
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     useShaderProgram();
