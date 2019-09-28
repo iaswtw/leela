461398b35142e564010012ac40268aa9a4c49f09
Abhir Joshi <abhirjoshi@live.com>
2019-09-28 14:27:53 -0500

    Limits on how fast time can increment.

--- ucore/Universe.h
+++ ucore/Universe.h
@@ -197,6 +197,8 @@ public:
     void LookAtEarthFromSavedVector();
     void LookAtSun();
 
+    void IncreaseSimulationSpeed();
+    void DecreaseSimulationSpeed();
     void SetSimulationSpeed(int nParam);
     void SetTimeDirection(int nParam);
     void FastForward(int nParam);
