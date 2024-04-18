8beb5ad420b58fc139a13972a4050fb2ee566eef
Abhir Joshi <abhirjoshi@live.com>
2024-04-18 04:37:55 -0500

    Manual earth and moon revolution using function keys is now allowed in simulation paused mode.

--- leela/Components/SimpleSphere.h
+++ leela/Components/SimpleSphere.h
@@ -13,6 +13,17 @@ public:
 	virtual void advance(float stepMultiplier) {}
 
 	virtual glm::mat4 getPositionTransform() {
+		glm::mat4 mat(1.0f);
+
+		//-------------------------------------------------------------------
+		// Transformation matrix is created in reverse order of intended application to the transformed point.
+		//-------------------------------------------------------------------
+
+		// parent is assumed to have been updated.  Translate by an amount equal to the parent's position.
+		if (_sceneParent != nullptr)
+			mat = _sceneParent->getPositionTransform();
+
+		// TODO transform this simple sphere from t he parent's position.
 
 	}
 
