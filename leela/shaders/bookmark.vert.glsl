e8226b8c0e33a995e074dd3743512df66690c3d2
Abhir Joshi <abhirjoshi@live.com>
2024-04-21 17:05:45 -0500

    Implement lighting in fragment shader.  As light saturates, it spills into other components, making the color go towards white.

--- leela/shaders/bookmark.vert.glsl
+++ leela/shaders/bookmark.vert.glsl
@@ -8,20 +8,16 @@ uniform mat4 projection;
 uniform vec3 offset;
 
 out vec4 Color;
+out vec3 Normal;
+out vec3 FragPos;
 
 void main()
 {
     vec3 offsetPosition = (1 * position) + offset;
-    vec3 eye = vec3(1000, 1000, -2000) + offset;
 
-    vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(eye - offsetPosition);
-
-    float ambient = 0.1;
-    float diffuse = max(dot(norm, lightDir), 0.0);
-    
-    
     // apply all 3 transformations to the original point
     gl_Position = projection * vec4(offsetPosition, 1.0);
-    Color = (ambient + diffuse) * in_color;
+    FragPos = offsetPosition;
+    Normal = normal;
+    //Normal = mat3(transpose(inverse(model))) * aNormal;  
 }
